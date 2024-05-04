32e915a61235aaa4b53d3c86075545b2ced5ae77
Shumaila Waheed <162050091+ShumailaWaheed@users.noreply.github.com>
2024-05-04 17:43:20 +0500

    100 Days of Coding, Day 20 Challenge

---
+++ main.js
@@ -0,0 +1,32 @@
+"use strict";
+// Question: 58
+// Calculate the sum of all scores
+function averageScore(...scores) {
+    // Calculate the average score
+    let total = scores.reduce((sum, score) => sum + score, 0);
+    return total / scores.length;
+}
+// Example
+console.log(averageScore(84, 92, 88, 75, 93));
+// Question 59
+// make a function
+function createAdder(specialNumber) {
+    return function (number) {
+        return number + specialNumber;
+    };
+}
+// Question: 60
+// The user's details are kept inside
+let userProfile = (function () {
+    // The user's details are kept inside
+    let name = "Urooj";
+    let age = 25;
+    // This part shares the user's details
+    return {
+        displayInfo: function () {
+            console.log(`Name: ${name}, Age: ${age}`);
+        }
+    };
+})();
+// It says the user's name and age
+userProfile.displayInfo();
